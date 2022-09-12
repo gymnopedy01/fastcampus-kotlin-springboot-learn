969d55ffbd9c2bdcfc73449f5389366965ead30d
Taehyun Oh <gymnopedy01@gmail.com>
2022-09-12 20:36:15 +0900

    Initial commit

---
+++ src/test/kotlin/com/fastcampus/fastcampusspringboot/FastcampusSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusSpringbootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
